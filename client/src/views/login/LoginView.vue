837fa03ab0fc3766f393d705fcfe98ca58323fb8
Jaewon Kim <59272854+jaewon4@users.noreply.github.com>
2022-09-09 17:58:29 +0900

    update: login화면 input type수정

--- client/src/views/login/LoginView.vue
+++ client/src/views/login/LoginView.vue
@@ -6,7 +6,7 @@
         <h2 class="logo_name">빵떠리</h2>
       </a>
       <input type="text" class="input_id" placeholder="아이디" />
-      <input type="text" class="input_pw" placeholder="비밀번호" />
+      <input type="password" class="input_pw" placeholder="비밀번호" />
       <button class="btn_login">로그인</button>
       <button class="btn_sign_up">회원가입</button>
       <div class="id_pw_search">
