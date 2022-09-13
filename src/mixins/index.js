6354d964d65ec787f2718660fe65804b0d6cf73b
Jaewon Kim <59272854+jaewon4@users.noreply.github.com>
2022-09-13 17:38:55 +0900

    add: SignupUserView 생성

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -19,6 +19,21 @@ export default {
           console.log(e)
         })
       ).data
+    },
+    async $post(url, data) {
+      return await axios.post(url, data).catch((e) => {
+        console.log(e)
+      })
+    },
+    async $put(url, data) {
+      return await axios.put(url, data).catch((e) => {
+        console.log(e)
+      })
+    },
+    async $delete(url) {
+      return await axios.delete(url).catch((e) => {
+        console.log(e)
+      })
     }
   }
 }
