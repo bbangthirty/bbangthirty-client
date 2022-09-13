dbd51ee3e8a6c632ef6e6299172d274e0afaede3
Jaewon Kim <59272854+jaewon4@users.noreply.github.com>
2022-09-13 15:05:05 +0900

    add: sweetalert, loading 설치

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import mixin from './mixins'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
+import VueLoading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 
 import './assets/css/main.css'
 
@@ -10,4 +14,6 @@ const app = createApp(App)
 app.use(store)
 app.use(router)
 app.mixin(mixin)
+app.use(VueSweetalert2)
+app.use(VueLoading)
 app.mount('#app')
