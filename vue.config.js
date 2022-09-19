3431be44a913f874a2edfe4910bd57c9bd8f5d20
mojaeya <mojaeya@gmail.com>
2022-09-19 23:27:14 +0900

    update: local https 인증키 설정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    https: {
+      key: fs.readFileSync('localhost+2-key.pem'),
+      cert: fs.readFileSync('localhost+2.pem')
+    }
+  }
 })
