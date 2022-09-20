5b4d2ff0354506eedcf29aebb27c3947471aec3c
mojaeya <mojaeya@gmail.com>
2022-09-20 13:22:09 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
-const fs = require('fs')
+// const fs = require('fs')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    https: {
-      key: fs.readFileSync('localhost+2-key.pem'),
-      cert: fs.readFileSync('localhost+2.pem')
-    }
-  }
+  transpileDependencies: true
+  // devServer: {
+  //   https: {
+  //     key: fs.readFileSync('localhost+2-key.pem'),
+  //     cert: fs.readFileSync('localhost+2.pem')
+  //   }
+  // }
 })
