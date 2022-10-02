50e2a3316eb6cf37a7c012e114c3d2c2d8dd1ac7
Jaewon Kim <59272854+jaewon4@users.noreply.github.com>
2022-10-02 17:49:03 +0900

    add: HeaderLayout1Logined header 생성

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <header-layout-one v-if="$route.path === '/'" />
+  <header-layout-one-logined v-if="$route.path === '/user/landing/regist'" />
   <header-layout-two v-if="$route.path === '/dongne'" />
   <router-view />
   <footer-layout />
 </template>
 <script>
 import HeaderLayoutOne from '@/components/layouts/HeaderLayout1.vue'
+import HeaderLayoutOneLogined from '@/components/layouts/HeaderLayout1Logined.vue'
 import HeaderLayoutTwo from '@/components/layouts/HeaderLayout2.vue'
 import FooterLayout from '@/components/layouts/FooterLayout.vue'
 export default {
-  components: { HeaderLayoutOne, HeaderLayoutTwo, FooterLayout },
+  components: {
+    HeaderLayoutOne,
+    HeaderLayoutTwo,
+    FooterLayout,
+    HeaderLayoutOneLogined
+  },
   data() {
     return {
       sampleData: ''
@@ -19,6 +26,8 @@ export default {
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {}
+  methods: {
+    changeHeader() {}
+  }
 }
 </script>
---
+++ src/components/layouts/HeaderLayout1Logined.vue
@@ -0,0 +1,88 @@
+<template>
+  <header class="header">
+    <div class="header-section">
+      <a class="header-logo">
+        <img src="@/assets/img/logo2.png" alt="logo" />
+      </a>
+      <div class="profile_section">
+        <img src="@/assets/img/dummy_user_img.png" alt="" class="profile_img" />
+        <a href="">소보루짱 님</a>
+        <button class="header-btn-login" @click="goToLogin()">로그아웃</button>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    goToLogin() {
+      this.$router.push({ path: '/login' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  background-color: var(--color-bread-b);
+}
+
+.header-section {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 40px;
+  margin: auto;
+  max-width: 1000px;
+}
+
+.header-logo img {
+  width: 50px;
+  cursor: pointer;
+}
+
+.header-logo-name {
+  margin: 0;
+}
+
+.header-btn-login {
+  border: none;
+  padding: 5px 10px;
+  background-color: var(--color-bread-d);
+  width: 100px;
+  border-radius: 10px;
+  color: white;
+}
+
+.header-btn-login:hover {
+  cursor: pointer;
+  background-color: var(--color-bread-c);
+}
+
+.profile_section {
+  display: flex;
+  align-items: center;
+  width: 250px;
+  justify-content: space-between;
+}
+
+.profile_img {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+
+a {
+  color: var(--color-bread-a);
+}
+</style>
